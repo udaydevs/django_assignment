937b752197591b56252d087d970a32089164b5b1
udaydevs <udaysinghno2005@gmail.com>
2025-12-27 01:13:48 +0530

    Applied validations

--- docx_app/form.py
+++ docx_app/form.py
@@ -17,22 +17,32 @@ class FormData(forms.Form):
     tele_no = forms.CharField(
         label="Telephone No.",
         validators=[RegexValidator(r'^[6-9]\d{9}$', "Enter valid 10-digit phone number.")]
-
     )
     mobile = forms.CharField(
         label="Mobile No.",
         required=False,
         validators=[RegexValidator(r'^[6-9]\d{9}$', "Enter valid 10-digit phone number.")]
     )
-    email = forms.EmailField(label="Email ID", required=False)
-    customer_name = forms.CharField(label="Customer Name")
+    email = forms.EmailField(
+        label="Email ID",
+        required=True
+    )
+    customer_name = forms.CharField(
+        label="Customer Name",
+        validators=[RegexValidator(r'^[A-Za-z\s]{2,50}$', "Only letters allowed.")]
+    )
     customer_address = forms.CharField(
         label="Customer Address",
         required=False
     )
-    customer_tele_no = forms.CharField(label="Customer Telephone No.")
+    customer_tele_no = forms.CharField(
+        label="Customer Telephone No.",
+        validators=[RegexValidator(r'^[6-9]\d{9}$', "Enter valid 10-digit phone number.")]
+
+    )
     customer_mobile_no = forms.CharField(
         label="Customer Telephone No.",
-        required=False
+        required=False,
+        validators=[RegexValidator(r'^[6-9]\d{9}$', "Enter valid 10-digit phone number.")]
     )
     customer_email = forms.EmailField(label="Customer Email Id")
