cdc96cce45c141810797935681ea37779aa71a28
udaydevs <udaysinghno2005@gmail.com>
2025-12-27 01:04:20 +0530

    Applied validations

--- docx_app/form.py
+++ docx_app/form.py
@@ -1,18 +1,28 @@
 """Fields for form"""
 from django import forms
+from django.core.validators import RegexValidator
+
 
 class FormData(forms.Form):
     """
     Docstring for MediationForm
     """
-    client_name = forms.CharField(label="Client Name")
+    client_name = forms.CharField(
+        label="Client Name",
+        validators=[RegexValidator(r'^[A-Za-z\s]{2,50}$', "Only letters allowed.")]
+    )
     branch_address = forms.CharField(
         label="Branch Address"
     )
-    tele_no = forms.CharField(label="Telephone No.")
+    tele_no = forms.CharField(
+        label="Telephone No.",
+        validators=[RegexValidator(r'^[6-9]\d{9}$', "Enter valid 10-digit phone number.")]
+
+    )
     mobile = forms.CharField(
         label="Mobile No.",
-        required=False
+        required=False,
+        validators=[RegexValidator(r'^[6-9]\d{9}$', "Enter valid 10-digit phone number.")]
     )
     email = forms.EmailField(label="Email ID", required=False)
     customer_name = forms.CharField(label="Customer Name")
